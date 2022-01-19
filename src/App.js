941ebd9d2abd603f6b8f075f9fe479801a5f305f
GinGerBread-Yellow <andyhuang0323@gmail.com>
2022-01-19 14:35:01 +0800

    fix: title and readme

--- src/App.js
+++ src/App.js
@@ -2,32 +2,11 @@
 import './App.css';
 import { useEffect, useState} from 'react'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
-// import Post from './containers/Post'
 import Login from './containers/Login'
-// import UserInterface from './containers/UserInterface';
-// import SignUp from './containers/SignUp';
-// import {PrivateRoute} from './components/PrivateRoute';
 import CarApp from './containers/CarApp';
 import SignUp from './containers/SignUp';
-import { getUserName } from './axios/axios';
 
 function App() {
-  const [isLoading, setLoading] = useState(false);
-  // const {username, isAuth, login, logout, autoLogin} = useAuth();
-  // useEffect(() => {
-    // do read cache here
-  //   setLoading(true)
-  //   if (!username) {
-  //     console.log("Welcome to Mui Mui");
-  //     console.log("try login automatically...");
-  //     autoLogin();
-  //   }
-  //   setLoading(false)
-  // }, []);
-
-  // if (isLoading) {
-  //   return <div><p>is loading...</p></div>;
-  // }
 
   return (
     
